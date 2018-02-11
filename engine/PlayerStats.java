de5ddc99aa1839511a5a66bfba0bf975f5225064
sonhal <sondre.hal@gmail.com>
2018-02-11 17:38:00 +0100

    Created classes: - GameObject - Item - Weapon - LivingObject - Avatar - PlayerStats - Sprite
    Created Enum:
    -Directions
    
    Added JavaDoc strings

---
+++ engine/PlayerStats.java
@@ -0,0 +1,11 @@
+package engine;
+
+import java.util.List;
+
+/**
+ * Represents the player Avatars stats during a game.
+ */
+public class PlayerStats {
+
+    private List<Weapon> weapons;
+}
--- view/MenuController.java
+++ view/MenuController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class MenuController implements Initializable{
 
     @FXML
